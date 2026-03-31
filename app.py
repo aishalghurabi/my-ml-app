f330a658ea3816bb49a1d65b2224a2a67cbfb481
aishalghurabi <alghurabiaisha@gmail.com>
2026-03-31 07:08:02 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
 import os
+from flask import Flask, render_template, request
 from model import predict
 
 app = Flask(__name__)
@@ -25,4 +25,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port)
